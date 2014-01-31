235a2aacc4ce673bc5048e1f5532389adb8827c7
lonnen <chris.lonnen@gmail.com>
2014-01-31 15:10:50 -0800

    testrunner and first test

---
+++ tests/config_test.js
@@ -0,0 +1,21 @@
+assert = require('assert');
+
+// poison environment before loading the config
+process.env['plugins'] = 'not a list'
+process.env['corsica-test'] = 'truthy';
+
+config = require('../lib/config.js');
+
+
+assert.ok(config['plugins'], 'does it read the default config?');
+
+assert.ok(
+  config['corsica-test'],
+  'does it read from the environment?');
+
+assert.strictEqual(
+  config.plugins,
+  'not a list',
+  'does the env override the default?'
+);
+
