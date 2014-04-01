d8f84c4e1dd0d8d4b1cf240ea5de049d67f899e9
Mike Cooper <mythmon@gmail.com>
2014-03-31 17:32:54 -0700

    Globbing.

---
+++ plugins/glob.js
@@ -0,0 +1,34 @@
+/* Description:
+ *   Expands screen name globs.
+ *
+ * Dependencies:
+ *   census
+ *
+ * Configuration:
+ *   None
+ *
+ * Author:
+ *    mythmon
+ */
+
+var Promise = require('es6-promise').Promise;
+var Minimatch = require('minimatch').Minimatch;
+
+module.exports = function (corsica) {
+  corsica.on('content', function (message) {
+    // If there is a * or a ? in the screen name, activate globbing.
+    // Otherwise, pass it on unmodified.
+    if ((message.screen || '').match(/[\*\?\+\|]/)) {
+      var pattern = Minimatch(message.screen);
+
+      return corsica.sendMessage('census.clients')
+      .then(function(data) {
+        message.screen = data.clients.filter(pattern.match.bind(pattern));
+        return message;
+      });
+
+    } else {
+      return message;
+    }
+  });
+};
