a254f5622f1185b07f41b0c9520d4944376fbab0
Mike Cooper <mythmon@gmail.com>
2014-04-03 10:52:18 -0700

    Make reset use commands.

--- plugins/reset.js
+++ plugins/reset.js
@@ -3,6 +3,10 @@
  *
  * Dependencies:
  *   settings
+ * 
+ * Optional Dependencies:
+ *   command - if present, the reset lines can be commands.
+ *   
  *
  * Configuration:
  *   defaultUrl
@@ -12,6 +16,8 @@
  */
 
 module.exports = function (corsica) {
+  var useCommand = corsica.config.plugins.indexOf('command') >= 0;
+
   var settings = corsica.settings.setup('reset', {
     defaultUrl: ['/default.html', 'http://xkcd.com'],
   });
@@ -20,12 +26,21 @@ module.exports = function (corsica) {
 
   corsica.on('reset', function(content) {
     return settings.get()
-      .then(function (settings) {
+    .then(function (settings) {
+      var nextLine = settings.defaultUrl[urlIndex];
+      urlIndex = (urlIndex + 1) % settings.defaultUrl.length;
+
+      if (useCommand) {
+        if ('screen' in content) {
+          nextLine += ' screen=' + content.screen;
+        }
+        corsica.sendMessage('command', {raw: nextLine});
+      } else {
         content.type = 'url';
-        content.url = settings.defaultUrl[urlIndex];
-        urlIndex = (urlIndex + 1) % settings.defaultUrl.length;
+        content.url = nextLine;
         corsica.sendMessage('content', content);
-        return content;
-      });
+      }
+      return content;
+    });
   });
 };
